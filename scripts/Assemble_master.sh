3ab45281684bb071237f033b4ff26adf2adfa781
Daniel Bunting <daniel.bunting14@imperial.ac.uk>
2017-04-14 15:24:00 +0100

    Prepare to merge LMP and PE pipelines

---
+++ scripts/Assemble_master.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#SBATCH --mem 4000
+#SBTACH -N 1
+#SBATCH -n 1
+#SBATCH -c 1
+#SBATCH -p tgac-medium
+
+
+cd /usr/users/ga004/buntingd/PSTGenome
+source /usr/users/ga004/buntingd/PSTGenome/genome/bin/activate
+export LUIGI_CONFIG_PATH=$SL/FP_pipeline/luigi.cfg
+
+srun python Assemble.py pe_libs.txt \
+            --base-dir $SL/PSTGenome/data \
+            --workers 100\
