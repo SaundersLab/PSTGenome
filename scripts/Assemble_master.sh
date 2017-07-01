77f0b968adfa28dfdabafd78f348cd2f93796874
Daniel Bunting <daniel.bunting14@imperial.ac.uk>
2017-07-01 11:58:38 +0100

    Update master script

--- scripts/Assemble_master.sh
+++ scripts/Assemble_master.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
-#SBATCH --mem 12000
+#SBATCH --mem 8000
 #SBTACH -N 1
 #SBATCH -n 1
 #SBATCH -c 1
-#SBATCH -p tgac-medium
+#SBATCH -p nbi-medium
 
 
 cd /usr/users/ga004/buntingd/PSTGenome
 source /usr/users/ga004/buntingd/PSTGenome/genome/bin/activate
-export LUIGI_CONFIG_PATH=$SL/FP_pipeline/luigi.cfg
+export LUIGI_CONFIG_PATH=$SL/FP_project/FP_pipeline/luigi.cfg
 
 srun python Assemble.py pe_libs.txt lmp_libs.txt \
             --K-list '[200, 216, 232, 240, 280, 300, 400, 500, 640]' \
             --base-dir $SL/PSTGenome/data \
-            --workers 100\
-            --no-lock
+            --workers 100
